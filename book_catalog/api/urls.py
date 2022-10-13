0af24d2ce565797db89ffa9c9fedf8e9ec689108
Vadim Ganitsa <vadimganica@gmail.com>
2022-10-13 13:06:51 +0600

    Add adding to favorites view

--- book_catalog/api/urls.py
+++ book_catalog/api/urls.py
@@ -4,4 +4,5 @@ from api import views
 urlpatterns = [
     path('books/', views.BookList.as_view()),
     path('books/<int:pk>/', views.BookDetail.as_view()),
+    path('books/<int:pk>/add-to-favorite/', views.AddToFavoritesView.as_view()),
 ]
--- book_catalog/api/views.py
+++ book_catalog/api/views.py
@@ -1,5 +1,9 @@
+from django.http import JsonResponse
+from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import generics
+from rest_framework.views import APIView
+
 from api.models import Book
 from api.serializers import BookSerializer, BookDetailSerializer
 
@@ -14,3 +18,13 @@ class BookDetail(generics.RetrieveAPIView):
     queryset = Book.objects.all()
     serializer_class = BookDetailSerializer
     permission_classes = [IsAuthenticated]
+
+
+class AddToFavoritesView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def patch(self, request, pk: int):
+        book = get_object_or_404(Book, pk=pk)
+        user = request.user
+        user.favorites.add(book)
+        return JsonResponse({"status": "true"})
